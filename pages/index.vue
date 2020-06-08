c93309342c5f61e747750e6f86351f446dbfc8c9
Patrick Labes <patrick.labes@gmail.com>
2020-06-09 07:17:09 +1000

    working on front end to display data

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="section">
-    <div class="columns is-mobile p-b-15">
+    <div class="columns is-mobile p-b-15" v-if="!dropFiles">
       <div class>
         <b-field>
           <b-upload accept=".csv, text/csv" v-model="dropFiles" drag-drop>
@@ -15,23 +15,35 @@
           </b-upload>
         </b-field>
       </div>
+    </div>
 
+    <div class="columns" v-if="dropFiles">
+      <div class="column">
+        <b-button @click="processCSV">{{hasBeenProcessed ? 'Re-' : ''}}Process CSV</b-button>
+      </div>
+      <div class="column" @click="exportCSV">
+        <b-button>Export CSV</b-button>
+      </div>
       <div class="column">
         <div class="tags">
-          <span class="tag is-primary" v-if="dropFiles">
+          <span class="tag is-primary">
             {{ dropFiles.name }}
             <button
               class="delete is-small"
               type="button"
-              @click="deleteDropFile(index)"
+              @click="deleteDropFile()"
             ></button>
           </span>
         </div>
       </div>
     </div>
 
-    <div class="columns" v-if="dropFiles">
-      <b-button @click="processCSV">Process CSV</b-button>
+    {{testCSV}}
+
+    <div class="columns" v-if="isLoading">
+      <div class="column">
+        <b-progress type="is-info"></b-progress>
+      </div>
     </div>
   </section>
 </template>
@@ -44,18 +56,30 @@ export default {
     return {
       dropFiles: null,
       isLoading: false,
-      data:null
+      data: null,
+      hasBeenProcessed: false,
+      testCSV:''
     };
   },
   methods: {
     dispatchStore(data) {
-      this.$store.dispatch("CSVdata/processCSV", data);
+      this.hasBeenProcessed = true;
+      this.isLoading = true;
+      this.testCSV = data
+      this.$store
+        .dispatch("CSVdata/processCSV", data)
+        .then(() => {
+          this.isLoading = false;
+        })
+        .catch(() => {
+          this.isLoading = false;
+        });
     },
     processCSV() {
       this.$papa.parse(this.dropFiles, {
         header: true,
         dynamicTyping: true,
-        complete: (results) => {
+        complete: results => {
           var data = results;
           //Data is capture
           _.forEach(data.data, (value, key) => {
@@ -63,7 +87,13 @@ export default {
           });
           this.dispatchStore(data.data);
         }
-      })
+      });
+    },
+    exportCSV() {
+      // TODO get data from getter 
+      let test = this.$papa.unparse(this.testCSV)
+       this.$papa.download(test, 'title')
+
     },
     debugDataset(results) {
       console.log(results);
@@ -73,7 +103,7 @@ export default {
       console.log(results);
       alert("results");
     },
-    deleteDropFile(index) {
+    deleteDropFile() {
       this.dropFiles = null;
     }
   }
